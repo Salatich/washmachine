10b9041297509235a766bda9eb69286ed127dc1d
Aleksandr <Salatiques@gmail.com>
2017-10-21 18:50:44 +0300

    init app

---
+++ src/main/java/appliance/washmachine/WashmachineApplication.java
@@ -0,0 +1,12 @@
+package appliance.washmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashmachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashmachineApplication.class, args);
+	}
+}
