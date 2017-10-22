27b4bcc2dceb69c337ffda26c12bd0f18c8f9e95
Aleksandr <Salatiques@gmail.com>
2017-10-23 02:06:10 +0300

    delete appliancerepository + oven class

--- src/main/java/appliance/washmachine/domain/ApplianceRepository.java
+++
@@ -1,6 +0,0 @@
-package appliance.washmachine.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ApplianceRepository extends CrudRepository<Appliance,Long> {
-}
